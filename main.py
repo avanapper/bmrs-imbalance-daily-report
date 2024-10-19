313172244013fa5000c8a2fe5e21d970ef35550b
AvaNapper <avanap1@gmail.com>
2024-10-19 15:35:08 +0100

    Initial commit, function to fetch data from API for given date

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd 
+
+def fetch_data_from_api_for_date(date):
+    '''
+    Fetch BMRS Imbalance data from Elexon Insights API for a given settlement date.
+
+    Parameters:
+    date : str
+        The settlement date for which to fetch the data, formatted as 'yyyy-mm-dd'.
+
+    Returns:
+    pd.DataFrame or None
+        A Pandas DataFrame containing the system price data for the specified 
+        date if the request was successful; otherwise, returns None.
+    '''
+    response = requests.get(f"https://data.elexon.co.uk/bmrs/api/v1/balancing/settlement/system-prices/{date}?format=json")
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        data = pd.DataFrame(data)
+        return data
+    else:
+        print("Error: ", response.status_code)
+        return None
+    
+date = "2024-02-01"
+df = fetch_data_from_api_for_date(date)
+print(df)
+
+    
+
