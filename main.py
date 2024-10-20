04bd1edba61d88b6160dcdd8b95cabe8b760ca8b
AvaNapper <avanap1@gmail.com>
2024-10-20 13:52:36 +0100

    Work in progress, created function to generate expected start times for the settlement date to filter out entries that don't match the settlement date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import pandas as pd 
-from datetime import datetime 
+from datetime import datetime, timedelta
 
 
 def fetch_data_from_api_for_date(date):
@@ -26,20 +26,48 @@ def fetch_data_from_api_for_date(date):
         print("Error: ", response.status_code)
         return None
     
+
+def generate_expected_start_times(date):
+    '''
+    Generate series of expected settlement period start times for given date.
+
+    Parameters:
+    date : str
+        The settlement date for which to generate the series, formatted as 'yyyy-mm-dd'.
+
+    Returns:
+    pd.DatetimeIndex
+        A Pandas DatetimeIndex containing series of timeslots at half hour intervals.
+    '''
+    start = f'{date} 00:00:00'  
+    end = f'{date} 23:30:00'    
+    interval = '0.5H'                     
+
+    date_series = pd.date_range(start = start, end = end, freq = interval)
+
+    return date_series
+
+
 date = "2024-04-01"
 df = fetch_data_from_api_for_date(date)
 filtered_data = df[['settlementDate', 'settlementPeriod', 'startTime', 'createdDateTime', 'systemSellPrice', 'systemBuyPrice', 'netImbalanceVolume']]
 
-
-
 # Some settlementDates are missing settlementPeriods and/or contain settlementPeriods from the previous date
-# 1. Remove entries where startTime doesn't match the settlementDate
-# 2. Bring in the misisng settlement Periods
-
+# 1. Remove entries where startTime doesn't match the settlementDate -- done
+# 2. Bring in the missing settlement Periods
 
 filtered_data['startTime'] = pd.to_datetime(filtered_data['startTime'])
 filtered_data['createdDateTime'] = pd.to_datetime(filtered_data['createdDateTime'])
-filtered_data['settlementDate'] = pd.to_datetime(filtered_data['settlementDate']).dt.date
-filtered_data = filtered_data[filtered_data['settlementDate']  == filtered_data['startTime'].dt.date]
+filtered_data['settlementDate'] = pd.to_datetime(filtered_data['settlementDate'])
 
-print(filtered_data)
+expected_start_times = generate_expected_start_times(date)
+
+filtered_data = filtered_data[filtered_data['startTime'].isin(expected_start_times)]
+
+missing_times = expected_start_times[~expected_start_times.isin(filtered_data['startTime'])]
+
+if len(missing_times) > 0 :
+    date_Date = datetime.strptime(date, "%Y-%m-%d")
+    yesterday = date_Date - timedelta(days=1)
+
+
